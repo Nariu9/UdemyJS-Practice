f40bd99a13d8418fb5f5744d20fbe9dc200d810e
amkprom <amkprom11@gmail.com>
2021-04-20 15:24:55 +0300

    practice2 finished

--- script.js
+++ script.js
@@ -10,28 +10,30 @@ const personalMovieDB = {
     privat: false
 };
 
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов ?', ''),
+        b = prompt('На сколько оцените его ?', '');
+
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Ok!');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
 if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
+} else if (personalMovieDB.count >= 30) {
     console.log('Вы киноман');
 } else {
     console.log('Произошла ошибка');
 }
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов ?', ''),
-        b = prompt('На сколько оцените его ?', '');
-    personalMovieDB.movies[a] = b;
-}
-
-// const a = prompt('Один из последних просмотренных фильмов ?', ''),
-//     b = prompt('На сколько оцените его ?', ''),
-//     c = prompt('Один из последних просмотренных фильмов ?', ''),
-//     d = prompt('На сколько оцените его ?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);

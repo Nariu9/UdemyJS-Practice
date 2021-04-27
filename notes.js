d754d360e750b8585b0ff640265e00a928264f83
amkprom <amkprom11@gmail.com>
2021-04-27 21:28:33 +0300

    lesson about arrays finished

--- notes.js
+++ notes.js
@@ -1,11 +1,26 @@
 'use strict';
 
-const arr = [1, 2, 3, 6, 8];
+const arr = [1220, 20, 35, 63, 8];
+arr.sort(compareNum);
+console.log(arr);
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+function compareNum(a, b) {
+    return a - b;
 }
 
-for (let value of arr) {
-    console.log(value);
-}
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+// // for (let value of arr) {
+// //     console.log(value);
+// // }
+
+// arr.forEach((item, i, arr) => {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));

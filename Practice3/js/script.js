24844ce5746189d36682585821368e1187db87f1
amkprom <amkprom11@gmail.com>
2021-06-09 22:17:15 +0300

    start practice8 adding timer on webpage

--- Practice3/js/script.js
+++ Practice3/js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    // Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -36,4 +38,24 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+
+    const deadline = '2020-06-11'; //Дата окончания акции
+
+    function getTimeRemaining(endtime) { //Создаём функцию для получения времени до конца акции
+        const t = Date.parse(endtime) - Date.parse(new Date()), //Получаем разницу между датой окончания акции и текущей датой 
+            days = Math.floor(t / (24 * 60 * 60 * 100)), //Вычисляем сколько это дней
+            hours = Math.floor((t / (60 * 60 * 1000)) % 24), // Вычисляем часы
+            minutes = Math.floor((t / 60 / 1000) % 60), //Вычисляем минуты
+            seconds = Math.floor((t / 1000) % 60); // Вычисляем секунды
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 });

8be096a8aa7aea7348fc3833577de91fecd73b0d
Macrage <stormblast213@gmail.com>
2022-02-06 20:35:36 +0200

    started a React project to create a kind of Twitter (react practice 1-3 finished)

---
+++ Project4/test-app/src/components/app/app.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+import AppHeader from '../app-header'
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            data : [
+                {label: 'Going to learn React', important: true, id: 1},
+                {label: 'It is nice', important: false, id: 2},
+                {label: 'I need a break ...', important: false, id: 3}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.maxId = 4;
+    }
+
+    deleteItem (id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data})=> {
+            const someArr = [...data, newItem];
+            return {
+                data: someArr
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div className='app'>
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                    posts={this.state.data} 
+                    onDelete={this.deleteItem}/>
+                <PostAddForm
+                onAdd = {this.addItem}/>
+            </div>
+        )
+    }
+}
